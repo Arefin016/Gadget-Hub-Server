29dda61641e00de0f73ad784660454f9fdcca822
Shah Arefin Ahmed <shaharefinahmed@gmail.com>
2024-08-17 03:46:49 +0600

    Pagination has been done

--- index.js
+++ index.js
@@ -37,6 +37,12 @@ async function run() {
       res.send(result)
     })
 
+    // reason for pagination
+    app.get("/allCategoriesCount", async (req, res) => {
+      const count = await allCategoriesCollection.estimatedDocumentCount()
+      res.send({ count })
+    })
+
     //specific data get from all categories
     app.get("/allCategories/:id", async (req, res) => {
       const id = req.params.id
